0248db1af3fcb8934074af3fcdb5df4a49f3d606
zhengjia <zhengjia@192.168.0.103>
2019-08-20 22:30:43 +0800

    index_keywords

---
+++ wlarein-ad-service/ad-search/src/main/java/com/wlarein/ad/index/keyword/UnitKeywoedObject.java
@@ -0,0 +1,13 @@
+package com.wlarein.ad.index.keyword;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UnitKeywoedObject {
+    private Long unitId;
+    private String Keyword;
+}
